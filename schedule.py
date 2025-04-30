2f29de1ec7f294228508bdd821ec43b781e90c42
Zihan Wu <wuzihandacongming@qq.com>
2025-04-30 23:11:40 +0800

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -1,22 +1,98 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from settings import *
-driver = webdriver.Chrome()
-driver.get("https://id-ouc-edu-cn-s.otrust.ouc.edu.cn/sso/login?service=https%3A%2F%2Fmy.ouc.edu.cn%2Fcas%2Flogin#/")
-# 登录界面
-in_username=(By.XPATH,'//*[@id="app"]/div/div[2]/div[3]/div/div/div[1]/div[1]/form/div[1]/div/div[1]/input')
-WebDriverWait(driver, 10).until(EC.presence_of_element_located(in_username)).send_keys(username)
-in_password=(By.XPATH, '//*[@id="app"]/div/div[2]/div[3]/div/div/div[1]/div[1]/form/div[2]/div/div/input')
-WebDriverWait(driver,10).until(EC.presence_of_element_located(in_password)).send_keys(password)
-in_button=(By.XPATH, '//*[@id="app"]/div/div[2]/div[3]/div/div/div[1]/div[1]/form/div[3]/div/button')
-WebDriverWait(driver,10).until(EC.presence_of_element_located(in_button)).click()
-in_search=(By.XPATH,'//*[@id="app"]/div/div[1]/div[3]/div/div/input')
-WebDriverWait(driver,10).until(EC.presence_of_element_located(in_search)).send_keys("教务系统")
-search_button=(By.XPATH,'//*[@id="app"]/div/div[1]/div[3]/div/div/span/span/i')
-WebDriverWait(driver,10).until(EC.presence_of_element_located(search_button)).click()
-enter_button=(By.CLASS_NAME,'el-image__inner')
-WebDriverWait(driver,10).until(EC.presence_of_element_located(enter_button)).click()
-time.sleep(5)
+from bs4 import BeautifulSoup
+
+
+def get_small_lists():
+    driver = webdriver.Chrome()
+    small_lists = []
+
+    try:
+        driver.get(
+            "https://id-ouc-edu-cn-s.otrust.ouc.edu.cn/sso/login?service=https%3A%2F%2Fmy.ouc.edu.cn%2Fcas%2Flogin#/")
+
+        # 登录流程
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="app"]/div/div[2]/div[3]/div/div/div[1]/div[1]/form/div[1]/div/div[1]/input'))
+        ).send_keys(username)
+
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="app"]/div/div[2]/div[3]/div/div/div[1]/div[1]/form/div[2]/div/div/input'))
+        ).send_keys(password)
+
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="app"]/div/div[2]/div[3]/div/div/div[1]/div[1]/form/div[3]/div/button'))
+        ).click()
+
+        # 搜索流程
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[1]/div[3]/div/div/input'))
+        ).send_keys("教务系统")
+
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[1]/div[3]/div/div/span/span/i'))
+        ).click()
+
+        # 进入系统
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'el-image__inner'))
+        ).click()
+
+        # 窗口切换
+        WebDriverWait(driver, 20).until(EC.number_of_windows_to_be(2))
+        new_window = [w for w in driver.window_handles if w != driver.current_window_handle][0]
+        driver.switch_to.window(new_window)
+
+        # Frame切换
+        driver.switch_to.default_content()
+        WebDriverWait(driver, 10).until(
+            EC.frame_to_be_available_and_switch_to_it((By.ID, "frmbody"))
+        )
+        WebDriverWait(driver, 10).until(
+            EC.frame_to_be_available_and_switch_to_it((By.ID, "frmDesk"))
+        )
+
+        # 数据解析
+        frame_html = driver.page_source
+        soup = BeautifulSoup(frame_html, 'html.parser')
+        tables = soup.find_all('table')
+
+        out = []
+        for table in tables:
+            for row in table.find_all('tr'):
+                for cell in row.find_all('td'):
+                    tmp = cell.string.replace('\n', '').replace('\t', '').replace(' ', '')
+                    out.append(tmp)
+
+        # 数据分组
+        front_list = out[:11]
+        small_lists = []
+        current_list = []
+
+        for item in out[11:]:
+            if item.isdigit() and len(current_list) > 0:
+                small_lists.append(current_list)
+                current_list = [item]
+            else:
+                current_list.append(item)
+        if current_list:
+            small_lists.append(current_list)
+
+        return small_lists[:12]
+
+    finally:
+        driver.quit()
+        print("浏览器已关闭")
+
+
+# 使用示例
+if __name__ == "__main__":
+    result = get_small_lists()
+    print("\n最终返回的小列表:")
+    print(result)
